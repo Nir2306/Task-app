f888974d837e2424b0088eb2486ac13b6185705c
Nirvesh-SC <nirvesh.jeewooth@spoonconsulting.com>
2025-11-06 23:43:10 +0400

    Fix Vercel build: Increase Workbox cache limit to 3MB for large bundle

--- vite.config.js
+++ vite.config.js
@@ -46,6 +46,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB (default is 2 MB)
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
